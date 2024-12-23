f289c9bc44cdc9fae17cf3d32ba9b50645fcd584
zmc_li <1912592745@qq.com>
2024-12-23 20:54:06 +0800

    1.安装瀑布流依赖

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import { setInterceptors } from './config'
 
-const url_py =  (import.meta.env.DEV == true) ? 'http://192.168.1.13:8081':'http://117.72.109.0:4289'
+// const url_py =  (import.meta.env.DEV == true) ? 'http://192.168.1.13:8081':'http://117.72.109.0:4289'
+const url_py = 'http://192.168.1.4:8081'
 const $http = axios.create({
   baseURL: url_py,
   timeout: 10000
