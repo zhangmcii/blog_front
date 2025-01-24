7a5742fe0432d3f22b9788a544e8717ab5c9d4b6
zmc <2797139657@qq.com>
2025-01-24 18:05:28 +0800

    1.删除 监听滚动条到页面底部加载评论的逻辑

--- src/components/AppLayout.vue
+++ src/components/AppLayout.vue
@@ -1,6 +1,5 @@
 <script>
 import AppHeader from './AppHeader.vue'
-import emitter from '@/utils/emitter.js'
 import { Scrollbar } from 'vue-amazing-ui'
 import 'vue-amazing-ui/es/scrollbar/Scrollbar.css'
 export default {
@@ -10,26 +9,6 @@ export default {
   },
   mounted() {},
   methods: {
-    handleScroll({ scrollLeft, scrollTop }) {
-      // 使用$refs获取el-scrollbar组件的根元素
-      const scrollbar = this.$refs.scrollbar;
-      
-      // 获取滚动容器的引用，el-scrollbar组件内部的滚动容器通常有一个类名为.el-scrollbar__wrap
-      const scrollbarWrap = scrollbar.$el.querySelector('.el-scrollbar__wrap');
-
-      // 滚动容器的总高度
-      const containerHeight = scrollbarWrap.scrollHeight;
-      // 滚动容器的可视高度
-      const visibleHeight = scrollbarWrap.clientHeight;
-
-      // 检查是否滚动到底部
-      if (scrollTop + visibleHeight >= containerHeight - 10) {
-        // 执行到底部的相关操作
-        emitter.emit('scroll')
-      }
-      console.log('111')
-    },
-    
   }
 }
 </script>
@@ -41,7 +20,7 @@ export default {
     </el-header>
     <el-divider />
     <el-main>
-      <Scrollbar ref="scrollbar" class="Scrollbar" @scroll="handleScroll">
+      <Scrollbar ref="scrollbar" class="Scrollbar">
         <router-view />
       </Scrollbar>
     </el-main>
