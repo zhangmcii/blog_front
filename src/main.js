73fb386f0740d340f124d23cc5c2dd6011e5e3cc
zmc <2797139657@qq.com>
2025-03-03 18:59:24 +0800

    1.复制指令

--- src/main.js
+++ src/main.js
@@ -15,23 +15,24 @@ import '@wangeditor/editor/dist/css/style.css'
 import { loadingFadeOut } from 'virtual:app-loading'
 loadingFadeOut()
 
-import vue3PhotoPreview from 'vue3-photo-preview';
-import 'vue3-photo-preview/dist/index.css';
+import vue3PhotoPreview from 'vue3-photo-preview'
+import 'vue3-photo-preview/dist/index.css'
 
-import { useElementPlus } from "@/plugins/elementPlus";
-import "element-plus/dist/index.css";
+import { useElementPlus } from '@/plugins/elementPlus'
+import 'element-plus/dist/index.css'
 
-import { useVant } from "@/plugins/vant";
+import { useVant } from '@/plugins/vant'
 import 'vant/lib/index.css'
-
+import {copy} from '@/directives/copy.js'
 const app = createApp(App)
 const pinia = createPinia()
 
 app.config.globalProperties.$dayjs = dayjs
 app.config.globalProperties.$message = ElMessage
+app.directive('copy', copy)
 app.use(useElementPlus)
 app.use(useVant)
 app.use(router)
 app.use(pinia)
-app.use(vue3PhotoPreview);
+app.use(vue3PhotoPreview)
 app.mount('#app')
