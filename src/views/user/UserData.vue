f29f387a28f690f1bb81563ac8cf583b7bf292da
zmc <2797139657@qq.com>
2025-05-07 18:51:44 +0800

    调整变量

--- src/views/user/UserData.vue
+++ src/views/user/UserData.vue
@@ -393,7 +393,7 @@ export default {
             v-slide-in
           >
             <template #image>
-              <PostImage :post_images="item.post_images" @click.stop="" />
+              <PostImage :postImages="item.post_images" @click.stop="" />
             </template>
           </PostPreview>
           <el-pagination
