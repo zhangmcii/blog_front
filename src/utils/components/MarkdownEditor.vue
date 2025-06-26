c207c81a94d9f1ddbe38c1254ca45e7410837fb9
zmc <2797139657@qq.com>
2025-06-26 19:01:33 +0800

    md图片上传

--- src/utils/components/MarkdownEditor.vue
+++ src/utils/components/MarkdownEditor.vue
@@ -1,71 +1,90 @@
 <template>
   <div class="markdown-editor">
-    <mavon-editor v-model="markdown" @imgAdd="handleImageUpload" @change="change" />
+    <mavon-editor
+      ref="mavonEditor"
+      v-model="markdown"
+      @imgAdd="handleImageUpload"
+      @change="change"
+    />
   </div>
 </template>
 
-<script setup>
-import { ref, computed, onBeforeMount } from 'vue'
+<script>
 import MarkdownIt from 'markdown-it'
+import { compressImages } from '@/utils/common.js'
+import uploadApi from '@/api/upload/uploadApi.js'
+import { v4 as uuidv4 } from 'uuid'
+import * as qiniu from 'qiniu-js'
+import lrz from 'lrz'
 
-const props = defineProps({
-  bodyInit: {
-    type: String,
-    default() {
-      return ''
+export default {
+  name: 'MarkdownEditor',
+  props: {
+    bodyInit: {
+      type: String,
+      default: () => ''
+    },
+    bodyHtmlInit: {
+      type: String,
+      default: () => ''
     }
   },
-  bodyHtmlInit: {
-    type: String,
-    default() {
-      return ''
+  emits: ['content_change'],
+  data() {
+    return {
+      markdown: '',
+      md: new MarkdownIt(),
+      // 原始文件
+      originalFiles: [],
+      // 压缩后的文件
+      compressedImages: [],
+      // 默认压缩比率为80%
+      compressedRatio: 80
+    }
+  },
+ 
+  mounted() {
+    this.markdown = this.bodyInit || ''
+  },
+  methods: {
+    debounce(func, wait) {
+      let timeout
+      return function (...args) {
+        const context = this
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
+          func.apply(context, args)
+        }, wait)
+      }
+    },
+    async handleImageUpload(pos, file) {
+      // 为每个文件生成唯一标识符
+      file.uid = uuidv4() 
+
+      console.log('图片上传:', pos, file)
+      // 图片上传逻辑可在此补充
+      // 例如上传后调用 this.$refs.mavonEditor.$img2Url(pos, imageUrl)
+
+      // 压缩图片
+      
+      // 确保只添加新的文件
+      const newFiles = fileList.filter(
+        (f) => !this.originalFiles.some((of) => of.lastModified === f.lastModified)
+      )
+      this.originalFiles = [...this.originalFiles, ...newFiles]
+      // console.log('文件列表:', this.originalFiles)
+       this.compressedImages = await compressImages(this.originalFiles, this.compressedImages)
+    },
+    change(value) {
+      this.$emit('content_change', {
+        body: value,
+        bodyHtml: this.md.render(value)
+      })
+    },
+    clean() {
+      this.markdown = ''
     }
   }
-})
-
-const emit = defineEmits(['content_change'])
-defineExpose({ clean })
-
-const md = new MarkdownIt()
-
-// 编辑器内容
-const markdown = ref('')
-
-onBeforeMount(() => {
-  markdown.value = props.bodyInit || ''
-})
-// 处理图片上传
-const handleImageUpload = async (pos, file) => {
-    console.log('图片上传:', pos, file)
-  // 创建 FormData 对象
-//   const formData = new FormData()
-//   formData.append('image', file)
-
-//   try {
-//     // 上传到后端
-//     const response = await fetch('/api/upload/image', {
-//       method: 'POST',
-//       body: formData
-//     })
-
-//     if (response.ok) {
-//       const result = await response.json()
-//       if (result.errno === 0 && result.data.length > 0) {
-//         const imageUrl = result.data[0]
-//         // 将图片 URL 插入到 Markdown 中
-//         this.$refs.mavonEditor.$img2Url(pos, imageUrl)
-//       }
-//     }
-//   } catch (error) {
-//     console.error('图片上传失败:', error)
-//   }
-}
-
-function change(value) {
-  emit('content_change', { body: value, bodyHtml: md.render(value) })
-}
-function clean() {
-  markdown.value = ''
 }
 </script>
 
